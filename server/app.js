bac007bae9514366be56decfd30c47922ead4c61
yakovliam <jacob@yakovliam.com>
2021-07-08 21:25:36 -0400

    :zap: Add env config

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
+const k8s = require('@kubernetes/client-node');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,5 +18,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const cluster = {
+    name: process.env['CLUSTER.NAME'],
+    server: process.env['CLUSTER.SERVER']
+};
+
+const user = {
+    name: process.env['USER.USERNAME'],
+    password: process.env['USER.PASSWORD'],
+};
+
+const context = {
+    name: process.env['CONTEXT.NAME'],
+    user: user.name,
+    cluster: cluster.name,
+};
+
+const kc = new k8s.KubeConfig();
+kc.loadFromOptions({
+    clusters: [cluster],
+    users: [user],
+    contexts: [context],
+    currentContext: context.name,
+});
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+
+console.log("Started server")
 
 module.exports = app;
